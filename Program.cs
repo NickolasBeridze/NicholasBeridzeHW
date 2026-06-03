08cc9230c998b410416c6c9f15ab34ea42fab085
NickolasTheNicka <brawlgemer48@gmail.com>
2026-06-04 00:36:22 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NicholasBeridzeHW
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Task 1
+            //Console.WriteLine("Enter Your Number");
+            //string numStr = Console.ReadLine();
+            //bool isValid = byte.TryParse(numStr,  out byte num);
+
+
+            //for (int j = 0; j <= 10; j++)
+            //{
+            //    int count = j;
+            //    Console.WriteLine($"{num} * {count} = {num * count}");
+
+
+            //}
+            #endregion
+
+            #region Task 2
+            //Console.WriteLine("Enter Number Of Rows");
+            //string rowsStr = Console.ReadLine();
+            //bool isValid = byte.TryParse(rowsStr, out byte numberOfRows);
+
+            //for (int i = 1; i <= numberOfRows; i++)
+            //{
+            //    for (int j = 1; j <= numberOfRows - i; j++)
+            //    {
+            //        Console.Write(" ");
+            //    }
+            //    for (int k = 1; k <= i; k++)
+            //    {
+            //        Console.Write("* ");
+            //    }
+            //    Console.WriteLine();
+            //}
+            #endregion
+
+            #region Task 3
+            //Console.WriteLine("Insert Number");
+            //string numString = Console.ReadLine();
+
+            //bool isValid = byte.TryParse(numString, out byte num);
+
+            //if (isValid)
+            //{
+            //    int sum = 0;
+
+            //    for (int i = 0; i <= num; i++)
+            //    {
+            //        if (i % 2 == 0) 
+            //        {
+            //            sum += i;
+            //        }
+            //    }
+
+            //    Console.WriteLine($"Sum of even numbers: {sum}");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Invalid number.");
+            //}
+
+            #endregion
+
+            #region Task 4 
+            //Console.WriteLine("Guess The Random Number Between 1 - 100");
+            //string input = Console.ReadLine();
+            //Random rand = new Random();
+            //int targetNumber = rand.Next(1, 101);
+            //int attempts = 0;
+            //bool mogeba = false;
+            //int.TryParse(input, out int guess);
+               
+            //while (!mogeba)
+            //{
+            //    attempts++;
+            //     if (guess == 67)
+            //    {
+            //        Console.WriteLine("SixSeven 20+20+20+7=67 AAAAAAAAAAAAAAAAAAAAAAAAA");
+            //    }
+            //    if (guess < targetNumber)
+            //    {
+            //        Console.WriteLine("Too low! Try again.");
+            //    }
+            //    else if (guess > targetNumber)
+            //    {
+            //        Console.WriteLine("Too high! Try again.");
+            //    }
+                
+            //    else
+            //    {
+            //        mogeba = true;
+            //        Console.WriteLine($"Congratulations! You've guessed the number {targetNumber} in {attempts} attempts.");
+            //    }
+            //    if (!mogeba)
+            //    {
+            //        input = Console.ReadLine();
+            //        int.TryParse(input, out guess);
+            //    }
+            //}
+
+            #endregion
+        }
+    }
+}
